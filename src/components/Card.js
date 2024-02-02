7165c132d5b5e4b3819a948f8ff608c6b5ae156a
alvorobyova <alena.vorobyevaa@gmail.com>
2024-02-02 13:08:27 +0300

    path to images for cards changed

--- src/components/Card.js
+++ src/components/Card.js
@@ -19,7 +19,7 @@ export class Card extends React.Component {
       <div className={'card ' + (this.props.isShowed ? 'opened' : 'closed')} onClick={this.cardClickHandler.bind(this, this.props.item)}>
 
         <div className="card-inner card-front">
-          <img src={'/images/' + this.props.item.image} alt={this.props.item.name}/>
+          <img src={'images/' + this.props.item.image} alt={this.props.item.name}/>
         </div>
 
         <div className="card-inner card-back">
